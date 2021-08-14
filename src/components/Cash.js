830a76b7324e657a8a4b48aedc6078fd077de50c
sjmog <s_morgan@me.com>
2021-08-15 01:41:11 +0200

    Layout correctly

--- src/components/Cash.js
+++ src/components/Cash.js
@@ -1,7 +1,21 @@
 const Cash = () => {
   return (
     <div className="Cash">
-      Cash
+      <div className="Cash__Row Cash__Total">
+        <label className="h5">Total</label>
+        <div className="Cash__Amount h2">$151,332</div>
+      </div>
+      <div className="Cash__Row">
+        <label className="h5">Income</label>
+        <div className="Cash__Amount h3">$0<span className="h5">per week</span></div>
+      </div>
+      <div className="Cash__Row">
+        <label className="h5">Expenditure</label>
+        <div className="Cash__Amount h3">$2,440<span className="h5">per week</span></div>
+      </div>
+      
+      
+      
     </div>
   )
 }
